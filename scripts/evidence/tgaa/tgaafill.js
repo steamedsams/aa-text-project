bfa9c998378bcc89e422deb86813569bca8b354f
Sam <96966065+steamedsams@users.noreply.github.com>
2026-02-22 16:13:08 -0800

    Update tgaafill.js

--- scripts/evidence/tgaa/tgaafill.js
+++ scripts/evidence/tgaa/tgaafill.js
@@ -1,11 +1,17 @@
 let tds = document.getElementsByTagName("td");
-const langs = 2;
+const langs = ["Japanese", "English"];
+
+function alreadyHasLanguage(tmp){
+    for(let i = 0; i < langs.length; i++){
+        if(tmp.classList.contains(langs[i])){
+            return true;
+        }
+    }
+    return false;
+}
 
 for (let x = 0; x < tds.length; x++) {
-    if (x % langs == 0) {
-        tds[x].classList.add("Japanese");
-    }
-    else if(x % langs == 1) {
-        tds[x].classList.add("English");
+    if(!alreadyHasLanguage(tds[x])){
+        tds[x].classList.add(langs[x % langs.length]);
     }
 }
